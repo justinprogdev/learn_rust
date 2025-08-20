f327a874009218db2156b368a78bbc2ccdf4eb61
justinprogdev <uebertech@gmail.com>
2025-08-20 03:04:03 +0000

    test

--- ch2/grep-lite/src/main.rs
+++ ch2/grep-lite/src/main.rs
@@ -4,12 +4,12 @@ fn main() {
     Every face, every shop, bedroom window, public-house, and 
     dark square is a picture feverishly turned-- in search of what?
     It is the same with books.
-    What do we seek through millions of pages?";
+    What do we seek through millions of pages? ";
 
     for (i, line) in quote.lines().enumerate() {
         if line.contains(search_term) {
             let line_number: usize = i + 1;
-            println!("{}: {}", line_number, line);
+            println!("{}: {}", line_number, line); 
         }
     }
 }
