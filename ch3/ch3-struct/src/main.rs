64eef3f6299687c437370ba3ea175aea056798d1
JustinProgDev <developmentjustin7@gmail.com>
2025-09-04 00:21:47 +0000

    completed ch3.2

--- ch3/ch3-struct/src/main.rs
+++ ch3/ch3-struct/src/main.rs
@@ -3,15 +3,40 @@
 
 struct File {
     name: String,
-    data: vec<u8>,
+    data: Vec<u8>,
+}
+
+fn open(f: &mut File) -> bool {
+    true
+}
+
+fn close(f: &mut File) -> bool {
+    true
+}
+
+fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
+    let mut tmp = f.data.clone();
+    let read_length = tmp.len();
+
+    save_to.reserve(read_length);
+    save_to.append(&mut tmp);
+    read_length
 }
 
 fn main() {
-    let f1 = File {
-        name: String ::from("f1.txt"),
-        data: Vec::new(),
+    let mut f2 = File {
+        name: String::from("2.txt"),
+        data: vec![114, 117, 115, 116, 33],
     };
 
-    let f1_name = &f1.name;
-    
+    let mut buffer: Vec<u8> = vec![];
+    open(&mut f2);
+    let f2_length = read(&f2, &mut buffer);
+    close(&mut f2);
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    print!("{:?}", f2);
+    print!("{} is {} bytes long", f2.name, f2_length);
+    print!("{}", text)
 }
